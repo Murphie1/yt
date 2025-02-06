b2d180720d885c3c7b6922b06a3d4a8b28749009
Isaias Malvar <isaiasmg@93hotmail.com>
2025-02-06 23:57:56 +0000

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-blue-50 text-red-400">
+      hello world - talwind working
+      <div>
+        <Button variant={"destructive"}>SHADCN WORKING</Button>
+      </div>
+    </div>
+  );
+}
