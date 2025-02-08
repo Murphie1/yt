ad55b3c99a66c2206e5181fbf35211b6ec710a1f
Isaias Malvar <isaiasmg@93hotmail.com>
2025-02-08 19:21:44 +0000

    Add personal section to home sidebar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="">
-      <Image src="youtube.svg" alt="web logo" height={50} width={50} />
-      <p className="text-xl font-semibold tracking-tight">NewTube</p>
-    </div>
-  );
+  return <div className="">I will load videos in the future!</div>;
 }
