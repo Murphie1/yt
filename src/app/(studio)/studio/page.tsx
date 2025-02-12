65017680bd44ee10e37d33ae3290834f59103919
Isaias Malvar <isaiasmg@93hotmail.com>
2025-02-12 21:39:45 +0000

    Create video schema, router and update db

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,7 +1,15 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { StudioView } from "@/modules/studio/ui/view/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
 import React from "react";
 
-const Page = () => {
-  return <div>Hi</div>;
+const Page = async () => {
+  void trpc.studio.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT });
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 };
 
 export default Page;
---
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { trpc } from "@/trpc/client";
+import { VideosSection } from "../sections/videos-section";
+import { DEFAULT_LIMIT } from "@/constants";
+
+export const StudioView = () => {
+  const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    { limit: DEFAULT_LIMIT },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    },
+  );
+  return (
+    <div>
+      {JSON.stringify(data)}
+      <VideosSection />
+    </div>
+  );
+};
