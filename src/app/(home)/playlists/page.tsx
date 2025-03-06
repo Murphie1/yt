8fe5ce8ff5c99177633242854ab016360cf406e5
Isaias Malvar <isaiasmg@93hotmail.com>
2025-03-06 23:17:33 +0000

    Create custom playlist page, router, procedure and apply api functionality to UI component

---
+++ src/app/(home)/playlists/page.tsx
@@ -0,0 +1,15 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { PlaylistsView } from "@/modules/playlists/ui/views/playlists";
+import { trpc } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
+
+const Page = async () => {
+  void trpc.playlists.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT });
+  return (
+    <HydrateClient>
+      <PlaylistsView />
+    </HydrateClient>
+  );
+};
+
+export default Page;
