6850d89a3575719081856ac77771fdf895aa5591
Murphie1 <130502671+Murphie1@users.noreply.github.com>
2025-03-17 05:44:22 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,44 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "oaidalleapiprodscus.blob.core.windows.net",
+            },
+            {
+                protocol: "https",
+                hostname: "utfs.io",
+            },
+            {
+                protocol: "https",
+                hostname: "ucarecdn.com",
+            },
+            {
+                protocol: "https",
+                hostname: "img.clerk.com",
+            },
+            {
+                protocol: "https",
+                hostname: "lovely-flamingo-139.convex.cloud",
+            },
+            {
+                protocol: "https",
+                hostname: "combative-meerkat-957.convex.cloud",
+            },
+        ],
+        domains: [
+            "uploadthing.com",
+            "res.cloudinary.com",
+            "avatars.githubusercontent.com",
+            "lh3.googleusercontent.com",
+        ],
+    },
+}
+
+export default nextConfig
+{/*import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
@@ -16,3 +56,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+*/}
